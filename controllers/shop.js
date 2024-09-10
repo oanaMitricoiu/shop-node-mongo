35eb3e002fccd073ba2594a9350a5168fc3d7ce7
oana-iu <oana.mitri01@gmail.com>
2024-09-10 16:40:42 +0300

    add functionality for a customer to place orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
     Product.find()
@@ -80,24 +81,44 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-    let fetchedCart;
     req.user
-        .addOrder()
+        .populate("cart.items.productId")
+        .execPopulate()
+        .then((user) => {
+            const products = user.cart.items.map((i) => {
+                return {
+                    quantity: i.quantity,
+                    product: { ...i.productId._doc },
+                };
+            });
+            const order = new Order({
+                user: {
+                    name: req.user.name,
+                    userId: req.user,
+                },
+                products: products,
+            });
+            return order.save();
+        })
         .then((result) => {
+            return req.user.clearCart();
+        })
+        .then(() => {
             res.redirect("/orders");
         })
-        .catch((err) => console.log(err));
+        .catch((error) => {
+            console.log(error);
+        });
 };
 
 exports.getOrders = (req, res, next) => {
-    req.user
-        .getOrders()
-        .then((orders) => {
-            res.render("shop/orders", {
-                path: "/orders",
-                pageTitle: "Your Orders",
-                orders: orders,
-            });
-        })
-        .catch((err) => console.log(err));
+  Order.find({ "user.userId": req.user._id })
+      .then((orders) => {
+          res.render("shop/orders", {
+              path: "/orders",
+              pageTitle: "Your Orders",
+              orders: orders,
+          });
+      })
+      .catch((err) => console.log(err));
 };
