d62c93a5cef00a2861a5e9c9aa1c44c44eef5d42
oana-iu <oana.mitri01@gmail.com>
2024-09-10 11:58:18 +0300

    add the user model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//     User.findById("5baa2528563f16379fc8a610")
-//         .then((user) => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById("66e009ba5149653c94622749")
+        .then((user) => {
+            req.user = user;
+            next();
+        })
+        .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -37,7 +37,21 @@ mongoose
         "mongodb+srv://oana:Mitricoiu01@cluster0.4esbfu7.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then((result) => {
-        console.log("Connected?");
+        console.log("Connected to mongo");
+        User.findOne().then((user) => {
+            if (!user) {
+                const user = new User({
+                    name: "Oana",
+                    email: "oana@oana.com",
+                    cart: {
+                        items: [],
+                    },
+                });
+
+                user.save();
+            }
+        });
+
         app.listen(3000);
     })
     .catch((err) => {
